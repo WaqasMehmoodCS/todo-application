052d6240766e191157d3b72381ed632d0c49818f
Waqas Mehmood <waqasmehmoodcs@gmail.com>
2025-03-20 01:08:00 +0500

    completed

---
+++ actions.php
@@ -0,0 +1,86 @@
+<?php
+include './db.php';
+
+
+if (isset($_POST["delete"])) {
+    $id = $_POST['deleteID'];  // Get ID from the form
+    $result = $con->query("DELETE FROM `task` WHERE `id`='$id'");
+    if (!$result) {
+        die("Query failed: " . $con->error);
+    }
+    header('location: index.php');
+    exit();
+}
+
+
+if (isset($_POST["updateStatus"])) {
+    $id = $_POST['updateStatusID'];  // Get ID from the form
+    $task = $_POST['task'];
+    $status = $_POST['status'];
+    $result = $con->query("UPDATE `task` SET `task`='$task',`status`='$status' WHERE `id` = '$id'");
+    if (!$result) {
+        die("Query failed: " . $con->error);
+    }
+    header('location: index.php');
+    exit();
+}
+
+
+
+if (isset($_POST["update"])) {
+    $id = $_POST['updateID'];  // Get ID from the form
+    $result = $con->query("SELECT `id`,`task`, `status` FROM `task` WHERE `id`='$id'");
+
+    if (!$result) {
+        die("Query failed: " . $con->error);
+    }
+
+
+    $row = $result->fetch_assoc();
+    $id = $row["id"];
+    $task = $row["task"];
+    $status = $row["status"];
+    include './header.php';
+    if ($row) {
+        ?>
+
+
+        <div class="container-md bg-primary bg-gradient flex-grow-1 overflow-hidden p-0">
+            <h1 class="text-light fw-bolder text-center text-uppercase user-select-none bg-dark py-4">
+                Todo Application
+            </h1>
+
+            <form action="actions.php" method="post">
+                <div class="w-50 m-auto d-flex flex-column gap-4">
+                    <div>
+                        <label class="form-label">Task</label>
+                        <input placeholder="<?php echo $task ?>" name="task" class="form-control">
+                    </div>
+                    <div>
+                        <label class="form-label">Status</label>
+                        <input placeholder="<?php echo $status ?>" name="status" class="form-control" />
+                    </div>
+                    <button class="btn btn-success mt-2" name="updateStatus">
+                        <input type="hidden" name="updateStatusID" value="<?php echo $id; ?>">
+                        <div class="d-flex justify-content-center align-items-center gap-2">
+                            <p class="m-0 p-0">Update</p>
+                            <i class="fa-solid fa-pen"></i>
+                        </div>
+                    </button>
+                </div>
+            </form>
+
+            <?php
+
+    } else {
+        echo "No task found with ID: $id";
+    }
+
+}
+
+?>
+
+
+
+
+    <?php include './footer.php' ?>
--- tasks.php
+++ tasks.php
@@ -7,9 +7,13 @@ $result = $con->query('SELECT * FROM `task`');
 if (!$result) {
     die("Query failed: " . $con->error);
 }
+
+
+
 ?>
 <div class="p-1 mt-3 rounded-1 flex-grow-1 overflow-auto m-auto" style="height: calc(100vh - 200px);">
     <div class="table-responsive user-select-none">
+
         <table class="table table-striped table-hover table-borderless table-primary align-middle">
             <thead class="table-light">
 
@@ -24,29 +28,40 @@ if (!$result) {
                 <?php
                 // Display data
                 while ($row = $result->fetch_assoc()) {
+                    $id = $row['id'];
+                    $task = $row['task'];
+                    $status = $row['status'];
+
                     ?>
                     <tr class="table-primary text-center">
 
-                        <td scope="row"><?php echo $row['id'] ?></td>
-                        <td><?php echo $row['task'] ?></td>
-                        <td><?php echo $row['status'] ?></td>
+                        <td scope="row"><?php echo $id; ?></td>
+                        <td><?php echo $task ?></td>
+                        <td><?php echo $status ?></td>
 
 
                         <td class="d-flex justify-content-center align-items-center gap-3">
-                            <button class="btn btn-sm btn-success"
-                                style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">
-                                <div class="d-flex justify-content-center align-items-center gap-1">
-                                    <p class="m-0 p-0">Update</p>
-                                    <i class="fa-solid fa-pen"></i>
-                                </div>
-                            </button>
-                            <button class="btn btn-sm btn-danger"
-                                style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">
-                                <div class="d-flex justify-content-center align-items-center gap-1">
-                                    <p class="m-0 p-0">Delete</p>
-                                    <i class="fa-solid fa-trash"></i>
-                                </div>
-                            </button>
+                            <form method="post" action="./actions.php">
+                                <button class="btn btn-sm btn-success" name="update"
+                                    style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">
+                                    <input type="hidden" name="updateID" value="<?php echo $id; ?>">
+                                    <div class="d-flex justify-content-center align-items-center gap-1">
+                                        <p class="m-0 p-0">Update</p>
+                                        <i class="fa-solid fa-pen"></i>
+                                    </div>
+                                </button>
+                            </form>
+                            <form method="post" action="./actions.php">
+                                <button class="btn btn-sm btn-danger" name="delete"
+                                    style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">
+                                    <input type="hidden" name="deleteID" value="<?php echo $id; ?>">
+                                    <div class="d-flex justify-content-center align-items-center gap-1">
+                                        <p class="m-0 p-0">Delete</p>
+                                        <i class="fa-solid fa-trash"></i>
+                                    </div>
+                                </button>
+                            </form>
+
                         </td>
                     </tr>
                     <?php
@@ -54,5 +69,6 @@ if (!$result) {
                 ?>
             </tbody>
         </table>
+
     </div>
 </div>
